5760417e6b36e7c0d846193d778fef0d66d9efba
Ravi-Raushan <krravi015@gmail.com>
2020-04-15 23:36:16 +0530

    [Ravi Raushan]Added test cases to test new feature

--- test/java/com/moodanalysertest/MoodAnalyserTest.java
+++ test/java/com/moodanalysertest/MoodAnalyserTest.java
@@ -1,14 +1,27 @@
 package com.moodanalysertest;
 
 import com.moodAnalyser.MoodAnalyser;
+import com.moodAnalyser.MoodAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenNullMoodShouldReturnHappy() {
+    public void givenEmptyMoodShouldThrowMoodAnalyserException(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try {
+            String mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals("EMPTY MOOD", e.getMessage());
+        }
+    }
+    @Test
+    public void givenNullMoodShouldThrowMoodAnalyserException(){
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+        try {
+            String mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals("NULL MOOD", e.getMessage());
+        }
     }
 }
