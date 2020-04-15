f228a182f1e3c453e76a2bbed10e5deae77c36e9
Ravi-Raushan <krravi015@gmail.com>
2020-04-16 01:22:55 +0530

    [Ravi Raushan]Added method to get object with prameterized constructor

--- main/java/com/moodAnalyser/MoodAnalyserFactory.java
+++ main/java/com/moodAnalyser/MoodAnalyserFactory.java
@@ -8,12 +8,29 @@ public class MoodAnalyserFactory {
     public static MoodAnalyser createMoodAnalyserObject(String className,Class parameterClass) throws MoodAnalyserException, IllegalAccessException, InstantiationException {
         try {
             Class<?> moodAnalyserClass = Class.forName(className);
-            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(parameterClass);
-            return (MoodAnalyser) moodAnalyserClass.newInstance();
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor();
+            return (MoodAnalyser) moodConstructor.newInstance();
         } catch (ClassNotFoundException e) {
             throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "NO_SUCH_CLASS_ERROR");
         } catch (NoSuchMethodException e) {
             throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "NO_SUCH_METHOD_ERROR");
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+    public static MoodAnalyser createMoodAnalyserObjectWithParametrizeConstructor(String className,Class parameterClass) throws MoodAnalyserException, IllegalAccessException, InstantiationException {
+        try {
+            Class<?> moodAnalyserClass = Class.forName(className);
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(parameterClass);
+            return (MoodAnalyser) moodConstructor.newInstance("i am in happy mood");
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "NO_SUCH_CLASS_ERROR");
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "NO_SUCH_METHOD_ERROR");
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
