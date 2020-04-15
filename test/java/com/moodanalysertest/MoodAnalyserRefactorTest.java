f8a73ac22d37d9ee4accc8f68a5c730ce92f10e3
Ravi-Raushan <krravi015@gmail.com>
2020-04-16 03:17:54 +0530

    [Ravi Raushan]Added test cases to test new feature

---
+++ test/java/com/moodanalysertest/MoodAnalyserRefactorTest.java
@@ -0,0 +1,34 @@
+package com.moodanalysertest;
+
+import com.moodAnalyser.MoodAnalyserException;
+import com.moodAnalyser.MoodAnalyserFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+public class MoodAnalyserRefactorTest {
+    @Test
+    public void givenHappyMessageUsingReflationWhenProperShouldReturnHappyMood(){
+        try {
+            Constructor moodAnalyserConstructor = MoodAnalyserFactory.getThisConstructor("com.moodAnalyser.MoodAnalyserRefactor",String.class);
+              Object obj = MoodAnalyserFactory.createMoodAnalyserObject(moodAnalyserConstructor,
+                                                  "i am in happy mood");
+              Object mood = MoodAnalyserFactory.invokeMethod(obj,"analyseMood");
+             Assert.assertEquals("HAPPY",mood);
+        } catch (MoodAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void givenHappyMessageWhenImproperMethodShouldThrowMoodAnalyserException(){
+        try {
+            Constructor moodAnalyserConstructor = MoodAnalyserFactory.getThisConstructor("com.moodAnalyser.MoodAnalyserRefactor",String.class);
+            Object obj = MoodAnalyserFactory.createMoodAnalyserObject(moodAnalyserConstructor,
+                    "i am in happy mood");
+            Object mood = MoodAnalyserFactory.invokeMethod(obj,"analysisOfMood");
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals("analysisOfMood NO_SUCH_METHOD",e.getMessage());
+        }
+    }
+}
