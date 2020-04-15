94172d57f65046828f17e238a3325694fe59d529
Ravi-Raushan <krravi015@gmail.com>
2020-04-16 04:19:34 +0530

    [Ravi Raushan]Added code for change moodAnalyser field dynamically

--- main/java/com/moodAnalyser/MoodAnalyserException.java
+++ main/java/com/moodAnalyser/MoodAnalyserException.java
@@ -3,7 +3,7 @@ package com.moodAnalyser;
 public class MoodAnalyserException extends Exception{
     enum ExceptionType{
         ENTERED_NULL,ENTERED_EMPTY,NO_SUCH_CLASS,NO_SUCH_METHOD,INSTANT_CREATION_ERROR,CLASS_ILLEGAL_ACCESS,
-        CLASS_INVOCATION_ERROR,METHOD_ILLEGAL_ACCESS,METHOD_INVOCATION_ERROR;
+        CLASS_INVOCATION_ERROR,METHOD_ILLEGAL_ACCESS,METHOD_INVOCATION_ERROR,NO_SUCH_FIELD,FIELD_ACCESSIBILITY_ERROR;
     }
     public ExceptionType type;
     public MoodAnalyserException(ExceptionType type,String message){
