911d9b0b056c59b941589ae77e475e9549d8c82b
Ravi-Raushan <krravi015@gmail.com>
2020-04-16 01:37:03 +0530

    [Ravi Raushan]Refactor MoodAnalyserFactory class

--- main/java/com/moodAnalyser/MoodAnalyserFactory.java
+++ main/java/com/moodAnalyser/MoodAnalyserFactory.java
@@ -8,16 +8,13 @@ public class MoodAnalyserFactory {
     public static MoodAnalyser createMoodAnalyserObject(String className,Class parameterClass) throws MoodAnalyserException, IllegalAccessException, InstantiationException {
         try {
             Class<?> moodAnalyserClass = Class.forName(className);
-            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor();
-            return (MoodAnalyser) moodConstructor.newInstance();
+            Constructor<?> moodConstructor = moodAnalyserClass.getConstructor(parameterClass);
+            return (MoodAnalyser) moodAnalyserClass.newInstance();
         } catch (ClassNotFoundException e) {
             throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "NO_SUCH_CLASS_ERROR");
         } catch (NoSuchMethodException e) {
             throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "NO_SUCH_METHOD_ERROR");
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
         }
-        return null;
     }
     public static MoodAnalyser createMoodAnalyserObjectWithParametrizeConstructor(String className,Class parameterClass) throws MoodAnalyserException, IllegalAccessException, InstantiationException {
         try {
